a0770dd568838c0b8c1281ff2cf77dff587524fe
Kate <akinyi180@gmail.com>
2019-12-18 15:28:34 +0300

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a&bd!xux76#xe0)4o8hww&$kf2fk0c(a8ak*q!p1xm^30xlzz+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 TINYMCE_DEFAULTS_CONFIG = {
     'height': 360,
